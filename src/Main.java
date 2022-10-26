7f45dc75220c9dcf9b93f7e6d87bfcfe7de4fa33
YuryShageev <schageev.yur@yandex.ru>
2022-10-26 22:28:06 +0300

    добавил вторую книгу с уже настроенными классами и свойствами Забыл "a" в слове Brazil

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         System.out.println("Author's Name = " + john.getFirstName() + john.secondName);//Геттер есть здесь
 
 
-        Book theBoysFromBrzil = new Book("The Boys From Brazil", 1976);
-        System.out.println("The name of the book = " + theBoysFromBrzil.bookName);
-        System.out.println("Published = " + theBoysFromBrzil.publicationYear);
+        Book theBoysFromBrazil = new Book("The Boys From Brazil", 1976);
+        System.out.println("The name of the book = " + theBoysFromBrazil.bookName);
+        System.out.println("Published = " + theBoysFromBrazil.publicationYear);
         Author ira = new Author("Ira", " Levin");
         System.out.println("Author's name = " + ira.getFirstName() + ira.secondName);
     }
