639960889d9107359a4c76baed998c68fd6893ac
YuryShageev <schageev.yur@yandex.ru>
2022-10-26 20:00:27 +0300

    Зубрим Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book oldMansWar = new Book();
+        oldMansWar.bookName = "Old Man's War";
+        System.out.println("oldMansWar.bookName = " + oldMansWar.bookName);
+        Author john = new Author();
+        john.firstName = "John";
+        john.secondName = "Scalzi";
+        System.out.println("john.firstName = " + john.firstName);
+        System.out.println("john.secondName = " + john.secondName);
     }
 }
