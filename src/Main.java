306c808f277a2f702e0229418405619c62c7ff32
YuryShageev <schageev.yur@yandex.ru>
2022-10-26 22:27:17 +0300

    добавил вторую книгу с уже настроенными классами и свойствами

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         System.out.println("oldMansWar.publicationYear = " + oldMansWar.publicationYear);
         Author john = new Author("John", " Scalzi");
         System.out.println("Author's Name = " + john.getFirstName() + john.secondName);//Геттер есть здесь
+
+
+        Book theBoysFromBrzil = new Book("The Boys From Brazil", 1976);
+        System.out.println("The name of the book = " + theBoysFromBrzil.bookName);
+        System.out.println("Published = " + theBoysFromBrzil.publicationYear);
+        Author ira = new Author("Ira", " Levin");
+        System.out.println("Author's name = " + ira.getFirstName() + ira.secondName);
     }
 }
