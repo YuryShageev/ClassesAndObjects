abc30a833f3024cc2dc8f4555650522e3e034cb1
YuryShageev <schageev.yur@yandex.ru>
2022-10-26 20:23:38 +0300

    Надо 1000 раз сделать, чтобы запомнить

--- src/Book.java
+++ src/Book.java
@@ -4,7 +4,7 @@ public class Book {
 
     int publicationYear;
 
-    public static Book nameTheBook(String bookName, int publicationYear) {
+    public static Book TheBook(String bookName, int publicationYear) {
         Book oldMansWar = new Book();
         oldMansWar.bookName = bookName;
         oldMansWar.publicationYear = publicationYear;
