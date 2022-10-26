4c9860851168df99f5873b39f246572425fdd1d2
YuryShageev <schageev.yur@yandex.ru>
2022-10-26 20:05:19 +0300

    Зубрим Объекты и классы. Пока ещё идёт нормально, но шестерёнки начинают буксовать)))

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Book oldMansWar = new Book();
         oldMansWar.bookName = "Old Man's War";
+        oldMansWar.publicationYear = 2005;
         System.out.println("oldMansWar.bookName = " + oldMansWar.bookName);
+        System.out.println("oldMansWar.publicationYear = " + oldMansWar.publicationYear);
         Author john = new Author();
         john.firstName = "John";
         john.secondName = "Scalzi";
