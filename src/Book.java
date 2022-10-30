c83d480490dbbaaa497c51e83505d6754177a488
YuryShageev <schageev.yur@yandex.ru>
2022-10-30 20:03:28 +0300

    добавил вторую книгу с уже настроенными классами и свойствами Забыл "a" в слове Brazil

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,25 @@
 public class Book {
 
-    String bookName;
+    private final String  bookName;
 
+    private final Author author;
     int publicationYear;
 
-    public Book(String bookName, int publicationYear) {
+    public Book(String bookName, Author author, int publicationYear) {
         this.bookName = bookName;
+        this.author = author;
         this.publicationYear = publicationYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
     }
 
     public void setPublicationYear(int publicationYear) {
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        Book oldMansWar = new Book("Old Man's War", 2005);//Здесь использовал конструктор с "this"
-        System.out.println("The name of the book = " + oldMansWar.bookName);
+
+        Author john = new Author("John", " Scalzi ");
+        Book oldMansWar = new Book("Old Man's War ", john, 2005);//Здесь использовал конструктор с "this"
+        System.out.println("The name of the book = " + oldMansWar.getBookName() + john.getFirstName() + john.getSecondName() + oldMansWar.getPublicationYear());
         System.out.println("Published = " + oldMansWar.publicationYear);
         oldMansWar.setPublicationYear(2006);// Поставил сеттер здесь
         System.out.println("oldMansWar.publicationYear = " + oldMansWar.publicationYear);
-        Author john = new Author("John", " Scalzi");
-        System.out.println("Author's Name = " + john.getFirstName() + john.getSecondName());//Геттер есть здесь
 
 
-        Book theBoysFromBrazil = new Book("The Boys From Brazil", 1976);
-        System.out.println("The name of the book = " + theBoysFromBrazil.bookName);
-        System.out.println("Published = " + theBoysFromBrazil.publicationYear);
-        Author ira = new Author("Ira", " Levin");
-        System.out.println("Author's name = " + ira.getFirstName() + ira.getSecondName());
+        Book theBoysFromBrazil = new Book("The Boys From Brazil", new Author(" Ira", "Levin "), 1970);
+
+        System.out.println(theBoysFromBrazil.getBookName() + theBoysFromBrazil.getAuthor() + theBoysFromBrazil.getPublicationYear());
     }
 }
